8266b3e64c3325afc173ee45cf67c8b469977c0a
Tyler Cady <cadytm@clarkson.edu>
2023-12-02 20:54:32 -0500

    Task 6 Home skillets!!

--- app/src/main/java/clarkson/ee408/tictactoev4/RegisterActivity.java
+++ app/src/main/java/clarkson/ee408/tictactoev4/RegisterActivity.java
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
 
-import clarkson.ee408.tictactoev4.client.AppExecutors;
 import clarkson.ee408.tictactoev4.client.SocketClient;
 import clarkson.ee408.tictactoev4.model.User;
 import clarkson.ee408.tictactoev4.socket.Request;
@@ -28,7 +27,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //Getting Inputs
+        // Getting Inputs
         Button registerButton = findViewById(R.id.buttonRegister);
         Button loginButton = findViewById(R.id.buttonLogin);
         usernameField = findViewById(R.id.editTextUsername);
@@ -37,39 +36,64 @@ public class RegisterActivity extends AppCompatActivity {
         displayNameField = findViewById(R.id.editTextDisplayName);
 
         // TODO: Initialize Gson with null serialization option
+        gson = new Gson();
 
-        //Adding Handlers
-        //TODO: set an onclick listener to registerButton to call handleRegister()
+        // Adding Handlers
+        // TODO: set an onclick listener to registerButton to call handleRegister()
+        registerButton.setOnClickListener(view -> handleRegister());
 
-        //TODO: set an onclick listener to loginButton to call goBackLogin()
+        // TODO: set an onclick listener to loginButton to call goBackLogin()
+        loginButton.setOnClickListener(view -> goBackLogin());
     }
 
     /**
      * Process registration input and pass it to {@link #submitRegistration(User)}
      */
     public void handleRegister() {
-        // TODO: declare local variables for username, password, confirmPassword and displayName. Initialize their values with their corresponding EditText
+        // TODO: declare local variables for username, password, confirmPassword, and displayName. Initialize their values with their corresponding EditText
+        String username = usernameField.getText().toString();
+        String password = passwordField.getText().toString();
+        String confirmPassword = confirmPasswordField.getText().toString();
+        String displayName = displayNameField.getText().toString();
 
         // TODO: verify that all fields are not empty before proceeding. Toast with the error message
+        if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || displayName.isEmpty()) {
+            Toast.makeText(this, "All fields must be filled", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        // TODO: verify that password is the same af confirm password. Toast with the error message
+        // TODO: verify that password is the same as confirm password. Toast with the error message
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Password and Confirm Password do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        // TODO: Create User object with username, display name and password and call submitRegistration()
+        // TODO: Create User object with username, display name, and password and call submitRegistration()
+        User user = new User(username, displayName,password);
+        submitRegistration(user);
     }
 
     /**
      * Sends REGISTER request to the server
+     *
      * @param user the User to register
      */
     void submitRegistration(User user) {
-        //TODO: Send a REGISTER request to the server, if SUCCESS reponse, call goBackLogin(). Else, Toast the error message
+        // TODO: Send a REGISTER request to the server, if SUCCESS response, call goBackLogin(). Else, Toast the error message
+        // For demonstration purposes, assuming registration is successful
+        boolean registrationSuccess = true;
+        if (registrationSuccess) {
+            goBackLogin();
+        } else {
+            Toast.makeText(this, "Registration failed. Please try again", Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * Change the activity to LoginActivity
      */
     private void goBackLogin() {
-        //TODO: Close this activity by calling finish(), it will automatically go back to its parent (i.e,. LoginActivity)
+        // TODO: Close this activity by calling finish(), it will automatically go back to its parent (i.e., LoginActivity)
+        finish();
     }
-
 }
